5a93a5f22b2a9551e3af96a425e5f079c3071d6b
LeticiaGondo <legondo@outlook.com>
2026-02-04 20:14:24 -0300

    refactor: mockar servico de agendamento

--- src/main/java/br/com/fiap/hackathon/tea/infrastructure/gateway/AgendamentoGatewayImpl.java
+++ src/main/java/br/com/fiap/hackathon/tea/infrastructure/gateway/AgendamentoGatewayImpl.java
@@ -3,24 +3,21 @@ package br.com.fiap.hackathon.tea.infrastructure.gateway;
 import br.com.fiap.hackathon.tea.application.model.AgendamentoResponse;
 import br.com.fiap.hackathon.tea.application.port.AgendamentoGateway;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClient;
+
+import java.time.LocalDateTime;
 
 @Component
 public class AgendamentoGatewayImpl implements AgendamentoGateway {
 
-    private final RestClient restClient;
-
-    public AgendamentoGatewayImpl() {
-        this.restClient = RestClient.builder()
-                .baseUrl("http://localhost:9090")
-                .build();
-    }
 
     @Override
     public AgendamentoResponse agendar(String protocolo) {
-        return restClient.post()
-                .uri("/agendamento/{protocolo}", protocolo)
-                .retrieve()
-                .body(AgendamentoResponse.class);
+        return new AgendamentoResponse(
+                protocolo,
+                "AGENDADO",
+                LocalDateTime.now().plusDays(3),
+                "UBS Central",
+                "Investigação TEA"
+        );
     }
 }
